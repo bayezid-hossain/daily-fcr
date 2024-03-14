c439501256886bcf794e0dd61eaf7329564fa03c
forsaken <forsaken@shadow.lan>
2024-03-14 13:54:12 -0400

    skeleton : ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,233 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+const data = [
+  { age: 1, weight: 61, fcr: 0.207 },
+  { age: 2, weight: 79, fcr: 0.37 },
+  { age: 3, weight: 100, fcr: 0.499 },
+  { age: 4, weight: 123, fcr: 0.601 },
+  { age: 5, weight: 148, fcr: 0.682 },
+  { age: 6, weight: 177, fcr: 0.749 },
+  { age: 7, weight: 209, fcr: 0.803 },
+  { age: 8, weight: 244, fcr: 0.848 },
+  { age: 9, weight: 283, fcr: 0.887 },
+  { age: 10, weight: 325, fcr: 0.92 },
+  { age: 11, weight: 370, fcr: 0.949 },
+  { age: 12, weight: 419, fcr: 0.975 },
+  { age: 13, weight: 471, fcr: 0.999 },
+  { age: 14, weight: 527, fcr: 1.021 },
+  { age: 15, weight: 586, fcr: 1.042 },
+  { age: 16, weight: 648, fcr: 1.062 },
+  { age: 17, weight: 714, fcr: 1.082 },
+  { age: 18, weight: 782, fcr: 1.101 },
+  { age: 19, weight: 854, fcr: 1.12 },
+  { age: 20, weight: 928, fcr: 1.138 },
+  { age: 21, weight: 1006, fcr: 1.157 },
+  { age: 22, weight: 1085, fcr: 1.175 },
+  { age: 23, weight: 1168, fcr: 1.193 },
+  { age: 24, weight: 1252, fcr: 1.212 },
+  { age: 25, weight: 1339, fcr: 1.23 },
+  { age: 26, weight: 1428, fcr: 1.248 },
+  { age: 27, weight: 1518, fcr: 1.267 },
+  { age: 28, weight: 1661, fcr: 1.285 },
+  { age: 29, weight: 1704, fcr: 1.304 },
+  { age: 30, weight: 1799, fcr: 1.32 },
+  { age: 31, weight: 1895, fcr: 1.341 },
+  { age: 32, weight: 1992, fcr: 1.36 },
+];
+import React, { useState } from 'react';
+import StandardTable from './components/standardTable';
+
+const calculateFCR = (totalFeed: number, avgWeight: number): number => {
+  // Calculate FCR using the formula
+  // FCR = Total Feed / (Total Weight Gain)
+  return totalFeed / avgWeight;
+};
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    farmerName: '',
+    dlCode: '',
+    location: '',
+    totalDOCInput: '',
+    strain: '',
+    age: '',
+    todayMortality: '',
+    totalMortality: '',
+    avgWeight: '',
+    totalFeed: '',
+    farmStock: '',
+    disease: '',
+    medicine: '',
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    // Calculate FCR
+    const fcr = calculateFCR(
+      Number(formData.totalFeed),
+      Number(formData.avgWeight)
+    );
+    alert(`FCR: ${fcr}`);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-white flex min-h-screen items-center justify-center p-12 flex-col">
+      <div className="flex flex-col items-center mb-12">
+        <h1 className="bg-black text-black font-bold text-lg px-4 py-2 mb-4">
+          Daily FCR Calculator
+        </h1>
+        <div>
+          <div className="flex flex-col mb-4">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col items-center text-black"
+            >
+              <label htmlFor="farmerName">Farmer Name</label>
+              <input
+                type="text"
+                id="farmerName"
+                name="farmerName"
+                value={formData.farmerName}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="dlCode">DL Code</label>
+              <input
+                type="text"
+                id="dlCode"
+                name="dlCode"
+                value={formData.dlCode}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="location">Location</label>
+              <input
+                type="text"
+                id="location"
+                name="location"
+                value={formData.location}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="totalDOCInput">Total DOC Input</label>
+              <input
+                type="text"
+                id="totalDOCInput"
+                name="totalDOCInput"
+                value={formData.totalDOCInput}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="strain">Strain</label>
+              <input
+                type="text"
+                id="strain"
+                name="strain"
+                value={formData.strain}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="age">Age (in days)</label>
+              <input
+                type="text"
+                id="age"
+                name="age"
+                value={formData.age}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="todayMortality">Today Mortality</label>
+              <input
+                type="text"
+                id="todayMortality"
+                name="todayMortality"
+                value={formData.todayMortality}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="totalMortality">Total Mortality</label>
+              <input
+                type="text"
+                id="totalMortality"
+                name="totalMortality"
+                value={formData.totalMortality}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="avgWeight">Avg. Weight (in gm)</label>
+              <input
+                type="text"
+                id="avgWeight"
+                name="avgWeight"
+                value={formData.avgWeight}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="totalFeed">Total Feed</label>
+              <input
+                type="text"
+                id="totalFeed"
+                name="totalFeed"
+                value={formData.totalFeed}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="farmStock">Farm Stock</label>
+              <input
+                type="text"
+                id="farmStock"
+                name="farmStock"
+                value={formData.farmStock}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="disease">Disease</label>
+              <input
+                type="text"
+                id="disease"
+                name="disease"
+                value={formData.disease}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <label htmlFor="medicine">Medicine</label>
+              <input
+                type="text"
+                id="medicine"
+                name="medicine"
+                value={formData.medicine}
+                onChange={handleChange}
+                className="border border-black"
+              />
+
+              <button
+                type="submit"
+                className="bg-blue-500 font-bold px-4 py-2 mt-4 rounded border border-black text-black"
+              >
+                Calculate FCR
+              </button>
+            </form>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <StandardTable />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    backgroundColor: {
+      white: '#ffffff', // You can also use 'white' if you want to refer to it by name
+    },
+    screens: {
+      xl: { max: '1200px' },
+      lg: { max: '991px' },
+      md: { max: '767px' },
+      sm: { max: '550px' },
+      odd: { max: '403px', min: '376px' },
+      xsm: { max: '375px' },
     },
   },
   plugins: [],
