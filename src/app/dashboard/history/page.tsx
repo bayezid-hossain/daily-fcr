7fb9429ce30622fdfb2cae9f0e28864b61cee78f
forsaken <forsaken@shadow.lan>
2024-03-17 14:12:27 -0400

    domain name fixed

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -30,6 +30,7 @@ export default async function LoginPage() {
 async function loader() {
   // Fetch data from external API
   try {
+    console.log(cookies().get('token')?.value);
     const response = await axios.get(`${process.env.DOMAIN}/api/data/dates`, {
       withCredentials: true,
       headers: { token: cookies().get('token')?.value },
