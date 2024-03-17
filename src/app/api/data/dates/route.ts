7cac6d8f8d33671c07d0699090a1c55848cab3a9
forsaken <forsaken@shadow.lan>
2024-03-17 14:51:35 -0400

    domain name fixed

--- src/app/api/data/dates/route.ts
+++ src/app/api/data/dates/route.ts
@@ -9,9 +9,7 @@ export async function GET(request: NextRequest) {
   let verifiedToken;
   let cookie = request.headers.get('Cookie');
   try {
-    const token =
-      request.headers.get('token') ||
-      cookie?.substring(cookie.lastIndexOf('token='));
+    const token = cookie?.substring(cookie.lastIndexOf('token='));
     verifiedToken =
       token &&
       (await verifyAuth(token).catch((err: any) => {
